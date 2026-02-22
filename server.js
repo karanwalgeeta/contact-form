7d4b282bc2b0a3bbe2f65096ec8b170dd6a7d78e
karanwalgeeta <karanwalgeeta@gmail.com>
2026-02-22 12:44:06 +0530

    pitt

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.post("/contact", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
